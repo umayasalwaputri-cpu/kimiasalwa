f6e72d1a563127a107988dda2e70aa41a6e00c3f
umayasalwaputri-cpu <umayasalwaputri@gmail.com>
2026-05-22 14:15:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈Teknik Kimia apps")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+pip install streamlit[charts]
